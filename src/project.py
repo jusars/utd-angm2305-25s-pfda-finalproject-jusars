c25cd73b869921d63c4813afa45a685491da371d
Jules Sarenas <shartbringer79@gmail.com>
2025-04-18 16:05:34 -0500

    added fullscreen functionalty (hopefully)

--- src/project.py
+++ src/project.py
@@ -5,7 +5,8 @@ import pygame
 # general setup (initializing, window, etc)
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1920, 1080
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption('Legally Distinct Asteroids Game')
 running = True
 
 while running:
