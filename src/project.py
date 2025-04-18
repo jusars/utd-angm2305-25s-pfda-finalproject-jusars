41eac7302288f78328e0793522bbff9efdcb12d3
Jules Sarenas <shartbringer79@gmail.com>
2025-04-18 15:51:32 -0500

    working on window display

--- src/project.py
+++ src/project.py
@@ -6,3 +6,12 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1920, 1080
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # drawing the game
