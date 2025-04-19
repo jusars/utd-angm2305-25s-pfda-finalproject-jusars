b59ae68be2efccc5372f8d5e79519488e6f39c74
Jules Sarenas <shartbringer79@gmail.com>
2025-04-18 21:10:15 -0500

    deleted uneccessary surface

--- src/project.py
+++ src/project.py
@@ -10,15 +10,10 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.
 pygame.display.set_caption('Legally Distinct Asteroids Game')
 running = True
 
-# plain surface (not having this broke my code?)
-surf = pygame.Surface((100,200))
-surf.fill('gray')
-x = 100
-
 # importing images
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = ((WINDOW_WIDTH // 2), (WINDOW_HEIGHT // 2)))
-player_direction = 1
+player_direction = -1
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [ (random.randrange(0, WINDOW_WIDTH), random.randrange(0, WINDOW_HEIGHT)) for i in range(20)]
