af95ab48229361553f14ecb85817fca13c79e5f1
Jules Sarenas <shartbringer79@gmail.com>
2025-04-18 21:07:40 -0500

    making player bounce from left/right of screen

--- src/project.py
+++ src/project.py
@@ -18,6 +18,7 @@ x = 100
 # importing images
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = ((WINDOW_WIDTH // 2), (WINDOW_HEIGHT // 2)))
+player_direction = 1
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [ (random.randrange(0, WINDOW_WIDTH), random.randrange(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -27,6 +28,7 @@ meteor_rect = meteor_surf.get_frect(center = ((WINDOW_WIDTH // 2), (WINDOW_HEIGH
 
 laser_surf = pygame.image.load(join('image', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20,(WINDOW_HEIGHT - 20)))
+
 while running:
     # event loop
     # ESCAPE key to quit game
@@ -40,11 +42,15 @@ while running:
 
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
-    if player_rect.right < WINDOW_WIDTH:
-        player_rect.left += 0.1
+
     
     display_surface.blit(laser_surf, laser_rect)
     display_surface.blit(meteor_surf, meteor_rect)
+
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right < WINDOW_WIDTH or player_rect.left <0:
+        player_direction *= -1
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
