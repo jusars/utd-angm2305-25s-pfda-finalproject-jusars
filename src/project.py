cc30d6501a8dd860a3e11a31e301c9cdbb46ec00
Jules Sarenas <shartbringer79@gmail.com>
2025-05-09 22:53:39 -0500

    defining functions that all handle different aspects of game

--- src/project.py
+++ src/project.py
@@ -6,11 +6,17 @@ from os.path import join
 """starting from almost scratch unfortunately (referring to a different tutorial),
 I got way too lost :("""
 
-
+def __init__(self):
+        self.main()
+        self.screen = pygame.display.set_mode(2560, 1440)
+def game_loop(self):
+      while True:
+            self._handle_input()
+            self._process_game_logic()
+            self.draw()
 def main():
     pygame.init()
     pygame.display.set_caption("Legally Distinct Asteroids Game")
-    resolution = (2560, 1440)
-    screen = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
+
     
     pygame.quit()
