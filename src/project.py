4c61863313588f83b76c6808a3e613c0b65306a2
Jules Sarenas <shartbringer79@gmail.com>
2025-05-09 22:57:32 -0500

    see previous (defining dif. functions that will eventually be called)

--- src/project.py
+++ src/project.py
@@ -6,17 +6,29 @@ from os.path import join
 """starting from almost scratch unfortunately (referring to a different tutorial),
 I got way too lost :("""
 
-def __init__(self):
-        self.main()
-        self.screen = pygame.display.set_mode(2560, 1440)
-def game_loop(self):
-      while True:
-            self._handle_input()
-            self._process_game_logic()
-            self.draw()
-def main():
-    pygame.init()
-    pygame.display.set_caption("Legally Distinct Asteroids Game")
+class AsteroidsGame:
+    def __init__(self):
+            self._init_pygame()
+            self.screen = pygame.display.set_mode(2560, 1440)
 
-    
-    pygame.quit()
+    def game_loop(self):
+        while True:
+                self._handle_input()
+                self._process_game_logic()
+                self.draw()
+
+    def _init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Legally Distinct Asteroids Game")
+
+    def _handle_input(self):
+        pass
+
+    def _game_logic(self):
+        pass
+
+    def _draw(self):
+        self.screen.fill((0, 0, 255))
+        pygame.display.flip()
+
+        pygame.quit()
