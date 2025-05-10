4bc29fb13a15b9cd4ffd9ec1dd3871df46aaba97
Jules Sarenas <shartbringer79@gmail.com>
2025-05-10 00:24:04 -0500

    making class for game objects (player, asteroids, anything that shows up. I THINK. currently generic/ unassigned. will be called later?)

--- src/AsteroidsGame/models.py
+++ src/AsteroidsGame/models.py
@@ -2,3 +2,6 @@ from pygame.math import Vector2
 
 class GameObject:
     def __init__(self, position, sprite, velocity)
+        self.position = Vector2(position)
+        self.sprite = spriteself.radius = sprite.get_width() / 2
+        self.velocity = Vector2(velocity)
