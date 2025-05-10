3b66e0bf01577a422325fc89f14ee87f9be04731
Jules Sarenas <shartbringer79@gmail.com>
2025-05-10 00:27:09 -0500

    fixed typos + defining collision + movement

--- src/AsteroidsGame/models.py
+++ src/AsteroidsGame/models.py
@@ -1,11 +1,19 @@
 from pygame.math import Vector2
 
 class GameObject:
-    def __init__(self, position, sprite, velocity)
+    def __init__(self, position, sprite, velocity):
         self.position = Vector2(position)
-        self.sprite = spriteself.radius = sprite.get_width() / 2
+        self.sprite = sprite
+        self.radius = sprite.get_width() / 2
         self.velocity = Vector2(velocity)
 
     def draw(self, surface):
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
+
+    def move(self):
+        self.position = self.positon + self.velocity
+
+    def collides_with(self, other_obj):
+        distance = self.position.distance_to(other_obj.position)
+        return distance < self.radius + other_obj.radius
