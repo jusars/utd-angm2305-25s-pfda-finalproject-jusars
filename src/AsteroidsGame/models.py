4b9bf2fdda12bdd7510513db1c895dc3c19a0bcc
Jules Sarenas <shartbringer79@gmail.com>
2025-05-10 23:52:05 -0500

    forgot to replace get_width with get_size lol

--- src/AsteroidsGame/models.py
+++ src/AsteroidsGame/models.py
@@ -4,7 +4,7 @@ class GameObject:
     def __init__(self, position, sprite, velocity):
         self.position = Vector2(position)
         self.sprite = sprite
-        self.radius = sprite.get_width()[0] / 2
+        self.radius = sprite.get_size()[0] / 2
         self.velocity = Vector2(velocity)
 
     def draw(self, surface):
