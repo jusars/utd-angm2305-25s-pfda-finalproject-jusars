83b7bec7c87e5284e95956f5ab9ed6d34f3847b1
Jules Sarenas <shartbringer79@gmail.com>
2025-05-11 16:36:59 -0500

    bugfixing

--- src/project.py
+++ src/project.py
@@ -123,8 +123,10 @@ while running:
 
     # update game object movements
     player.move()
+    for index, bullet in enumerate(playerBullets):
+         bullet.move()
 
-    # exit functionality
+    # exit functionality + other inputs
     for event in pygame.event.get():
           if event.type == pygame.QUIT or (
             event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
