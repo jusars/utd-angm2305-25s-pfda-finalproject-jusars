a720c25211cbdaa9efcaf05c034e92ed07ddbac1
Jules Sarenas <shartbringer79@gmail.com>
2025-05-11 03:54:22 -0500

    fixed typos + added functionality to player accelerating (moving forward)

--- src/project.py
+++ src/project.py
@@ -98,11 +98,11 @@ while running:
 
     # handling input
     keys_pressed = pygame.key.get_pressed()
-    if keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_A]:
+    if keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_a]:
          player.rotation(-1)
-    elif keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_D]:
+    elif keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_d]:
          player.rotation(1)
-    if keys_pressed[pygame.K_UP] or keys_pressed[pygame.K_W]:
+    if keys_pressed[pygame.K_UP] or keys_pressed[pygame.K_w]:
          player.accelerate()
 
     gameWindowUpdating()
