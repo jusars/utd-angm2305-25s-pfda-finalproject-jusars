e742cc179e55659e01c525faaca14bd2049e1b2f
Jules Sarenas <shartbringer79@gmail.com>
2025-05-11 01:09:47 -0500

    see prev.

--- src/project.py
+++ src/project.py
@@ -2,3 +2,21 @@ import sys, os
 import pygame
 from pygame import Vector2
 import random
+
+pygame.init()
+
+# utility
+
+# game objects
+
+# game settings variables
+
+# pygame display window
+
+# loading game assets
+
+# one off functions (loading gameobjects, etc)
+
+# game object lists
+
+# main game loop
