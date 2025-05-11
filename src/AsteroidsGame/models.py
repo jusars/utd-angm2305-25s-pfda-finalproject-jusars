048074ec3571659d82982c2bf5ade251d4af5c9f
Jules Sarenas <shartbringer79@gmail.com>
2025-05-11 00:11:54 -0500

    fix attempt 4 i really have no clue why get_width doesnt work

--- src/AsteroidsGame/models.py
+++ src/AsteroidsGame/models.py
@@ -5,7 +5,7 @@ class GameObject:
     def __init__(self, position, sprite, velocity):
         self.position = Vector2(position)
         self.sprite = sprite
-        self.radius = sprite.get_width() / 2
+        self.radius = sprite.get_height() / 2
         self.velocity = Vector2(velocity)
 
     def draw(self, surface):
