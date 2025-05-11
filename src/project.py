1da40ba0be85a56aa08385c3e5ffd57775f55ab9
Jules Sarenas <shartbringer79@gmail.com>
2025-05-11 01:51:40 -0500

    see prev

--- src/project.py
+++ src/project.py
@@ -7,11 +7,17 @@ pygame.init()
 
 # utility
 """tldr: function to load sprites (image) and resize when needed"""
-def gameimageload(imagefilepath, size):
+def gameImageLoad(imagefilepath, size):
         image = pygame.image.load(imagefilepath)
         image = pygame.transform.scale(image, (size[0], size[1]))
         return image
 
+def gameWindowUpdating():
+     # bg image
+     SCREEN.blit(BGIMG, (0,0))
+
+     pygame.display.update()
+     
 # game objects
 
 # game settings variables
@@ -20,11 +26,9 @@ SCREENHEIGHT = 1440
 # pygame display window
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("legally distinct asteroids game")
-iconImg = gameimageload('assets/spaceship.png', (20, 20))
-iconImg = pygame.transform.rotate(iconImg, -90)
-pygame.display.set_icon(iconImg)
 
 # loading game assets
+BGIMG = gameImageLoad('assets/space.png', (SCREENWIDTH, SCREENHEIGHT))
 
 # one off functions (loading gameobjects, etc)
 
@@ -40,5 +44,7 @@ while running:
             event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
         ):
             running = False
+
+    gameWindowUpdating()
 
         
