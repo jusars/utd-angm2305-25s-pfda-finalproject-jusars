95e0a2209ce36f49e3ed646ae4b5b1f0f9ec6e76
Jules Sarenas <shartbringer79@gmail.com>
2025-05-12 22:13:30 -0500

    again

--- src/project.py
+++ src/project.py
@@ -266,7 +266,7 @@ class Asteroid(Player):
 # game settings variables
 clock = pygame.time.Clock()
 screen_width = 1920
-screen_height = 1080
+screen_height = 1440
 object_rotation_speed = 2
 object_speed = 0.25
 stage = 0
